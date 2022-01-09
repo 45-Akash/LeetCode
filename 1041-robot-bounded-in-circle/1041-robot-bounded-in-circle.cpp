dc41320d40ba0182b116a44148f53868534f8f67
Akash <83642503+45-Akash@users.noreply.github.com>
2022-01-09 11:34:59 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (79.18%) - LeetHub

---
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool isRobotBounded(string instructions) 
+    {
+        char d = 'N';
+        int x=0, y=0;
+        for(int i=0; i<instructions.size(); i++)
+        {
+            char mov = instructions[i];
+            if(d=='N')
+            {
+                if(mov=='G')  y++;
+                if(mov=='L')  d='W';
+                if(mov=='R')  d='E';
+            }
+            else if(d=='E')
+            {
+                if(mov=='G')  x++;
+                if(mov=='L')  d='N';
+                if(mov=='R')  d='S';
+            }
+            else if(d=='W')
+            {
+                if(mov=='G')  x--;
+                if(mov=='L')  d='S';
+                if(mov=='R')  d='N';
+            }
+            else if(d=='S')
+            {
+                if(mov=='G')  y--;
+                if(mov=='L')  d='E';
+                if(mov=='R')  d='W';
+            }
+        }
+        
+        if((x==0 && y==0) || d != 'N') return true;
+        return false;
+    }
+};
