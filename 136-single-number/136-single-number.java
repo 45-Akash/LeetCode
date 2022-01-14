2971e07b80187360e49feb02c7e3fb30510e32fe
Akash <83642503+45-Akash@users.noreply.github.com>
2022-01-14 19:07:06 +0530

    Time: 5 ms (42.30%), Space: 39.1 MB (67.05%) - LeetHub

---
+++ 136-single-number/136-single-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int singleNumber(int[] nums)
+    {
+        Arrays.sort(nums);
+        
+        int i;
+        for(i=0; i<nums.length-1; i+=2)
+        {
+            if(nums[i]!=nums[i+1])
+            {
+                break;
+            }
+        }
+    return nums[i];
+    }
+}
