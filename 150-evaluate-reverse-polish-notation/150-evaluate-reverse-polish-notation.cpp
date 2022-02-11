a1f2fdec1ae1febb5287e4c768eec00a43d5fc96
Akash <83642503+45-Akash@users.noreply.github.com>
2022-02-11 10:28:33 +0530

    Time: 16 ms (46.00%), Space: 11.9 MB (60.05%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
@@ -1,5 +1,17 @@
 class Solution {
 public:
+    void operation(stack<int>&st, string s)
+    {
+        int n1 = st.top();
+        st.pop();
+        int n2 = st.top();
+        st.pop();
+        if(s=="add")     st.push(n2+n1);
+        if(s=="sub")     st.push(n2-n1);
+        if(s=="mul")     st.push(n2*n1);
+        if(s=="div")     st.push(n2/n1);
+        
+    }
     int evalRPN(vector<string>& tokens)
     {
         stack<int>st;
@@ -7,42 +19,10 @@ public:
         
         for(int i=0; i<tokens.size(); i++)
         {
-            if(tokens[i]=="+")
-            {
-                num1 = st.top();
-                st.pop();
-                num2 = st.top();
-                st.pop();
-                op = num2 + num1;
-                st.push(op);
-            }
-            else if(tokens[i]=="*")
-            {
-                num1 = st.top();
-                st.pop();
-                num2 = st.top();
-                st.pop();
-                op = num2 * num1;
-                st.push(op);
-            }
-            else if(tokens[i]=="-")
-            {
-                num1 = st.top();
-                st.pop();
-                num2 = st.top();
-                st.pop();
-                op = num2 - num1;
-                st.push(op);
-            }
-            else if(tokens[i]=="/")
-            {
-                num1 = st.top();
-                st.pop();
-                num2 = st.top();
-                st.pop();
-                op = num2 / num1;
-                st.push(op);
-            }
+            if(tokens[i]=="+")          operation(st,"add");
+            else if(tokens[i]=="*")     operation(st,"mul");
+            else if(tokens[i]=="-")     operation(st,"sub");
+            else if(tokens[i]=="/")     operation(st,"div");
             else
             {
                 int val = stoi(tokens[i]);
