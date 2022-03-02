c1faff6e1ce923b754f16342a8dae6150a8891d2
Akash <83642503+45-Akash@users.noreply.github.com>
2022-03-02 19:03:48 +0530

    Time: 936 ms (6.48%), Space: 256.2 MB (5.04%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -6,11 +6,12 @@ public:
         
         int longest = INT_MIN;
         int len;
+        string ans;
         for(int i=0; i<s.size(); i++)
         {
             unordered_map<char,int>mp;
-            string sub="";
             len = 0;
+            string sub="";
             for(int j=i; j<s.size(); j++)
             {
                 sub += s[j];
@@ -18,11 +19,16 @@ public:
                 {
                     mp[s[j]]++;
                     len++;
-                    if(len > longest)     longest = len;
+                    if(len > longest)     
+                    {
+                        longest = len;
+                        ans = sub;
+                    }
                 }
                 else       break;   
             }
         }
+        cout<<ans<<endl;               //for printing the resultant string
     return longest;
     }
 };
