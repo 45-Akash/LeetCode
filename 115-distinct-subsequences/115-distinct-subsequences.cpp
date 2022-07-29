d44b4ecab7b38aa8713eb44d911dacc671e2d8bb
Akash <83642503+45-Akash@users.noreply.github.com>
2022-07-29 09:08:38 +0530

    Time: 46 ms (69.99%), Space: 18.5 MB (58.75%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int distSub(string s1, string s2, int i, int j, vector<vector<int>>&dp)
+    int distSub(string &s1, string &s2, int i, int j, vector<vector<int>>&dp)
     {
         if(j<0)     return 1;
         if(i<0)     return 0;
@@ -22,32 +22,32 @@ public:
         int l2 = t.length();
         long mod = 1e9+1;
         
-        vector<vector<int>>dp(l1+1, vector<int>(l2+1, 0));
+        vector<vector<int>>dp(l1, vector<int>(l2, -1));
         
-        for(int i=0; i<=l2; i++)
-        {
-            dp[0][i] = 0;
-        }
-        for(int i=0; i<=l1; i++)
-        {
-            dp[i][0] = 1;
-        }
+//         for(int i=0; i<=l2; i++)
+//         {
+//             dp[0][i] = 0;
+//         }
+//         for(int i=0; i<=l1; i++)
+//         {
+//             dp[i][0] = 1;
+//         }
         
         
-        for(int i=1; i<=l1; i++)
-        {
-            for(int j=1; j<=l2; j++)
-            {
-                if(s[i-1] == t[j-1])
-                {
-                    dp[i][j] = dp[i-1][j-1]%mod + dp[i-1][j]%mod;
-                }
-                else
-                {
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
-        }
-        return dp[l1][l2];
+//         for(int i=1; i<=l1; i++)
+//         {
+//             for(int j=1; j<=l2; j++)
+//             {
+//                 if(s[i-1] == t[j-1])
+//                 {
+//                     dp[i][j] = dp[i-1][j-1]%mod + dp[i-1][j]%mod;
+//                 }
+//                 else
+//                 {
+//                     dp[i][j] = dp[i-1][j];
+//                 }
+//             }
+//         }
+        return distSub(s, t, l1-1, l2-1, dp);
     }
 };
