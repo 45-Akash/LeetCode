c1bf99c491189c4333d4afc3d24572a6315a5034
Akash <83642503+45-Akash@users.noreply.github.com>
2022-11-23 22:48:44 +0530

    Time: 2 ms (97.46%), Space: 42.6 MB (88.31%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,51 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board)
+    {
+        Map<Character,Integer>hmp = new HashMap<>();
+        for(int i=0; i<9; i++)
+        {
+            for(int j=0; j<9; j++)
+            {
+                if(board[i][j] != '.'){
+                if(hmp.containsKey(board[i][j]))  return false;
+                else                              hmp.put(board[i][j], 1);
+                }
+            }
+            hmp.clear();
+        }
+        
+        for(int i=0; i<9; i++)
+        {
+            for(int j=0; j<9; j++)
+            {
+                if(board[j][i] != '.'){
+                if(hmp.containsKey(board[j][i]))  return false;
+                else                              hmp.put(board[j][i], 1);
+                }
+            }
+            hmp.clear();
+        }
+        
+        for(int i=0; i<9; i += 3)
+        {
+            for(int j=0; j<9; j += 3)
+            {
+                int row = i+3;
+                int col = j+3;
+                for(int p=i; p<row; p++)
+                {
+                    for(int q=j; q<col; q++)
+                    {
+                        if(board[p][q] != '.'){
+                        if(hmp.containsKey(board[p][q]))  return false;
+                        else                              hmp.put(board[p][q], 1);
+                        }
+                    }
+                }
+              hmp.clear();  
+            }
+        }
+      return true;                                              
+                                                    
+    }
+}
