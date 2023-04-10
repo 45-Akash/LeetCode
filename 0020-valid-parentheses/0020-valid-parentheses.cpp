5f649c8171875979995fb72213957ac5caaffd71
Akash <83642503+45-Akash@users.noreply.github.com>
2023-04-10 07:40:07 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (56.25%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int pref(char c)
+        {
+            if(c == '(' || c==')') {return 1;}
+            else if(c == '[' || c==']') {return 2;}
+            return 3;
+            
+        }
+    bool isValid(string s)
+    {
+       int len = s.size();
+       stack<char>st;
+       for(int i=0; i<len; i++)
+       {
+           if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
+           st.push(s[i]);
+           }
+           
+           
+            if(s[i]==')' || s[i]==']' || s[i]=='}')
+            {
+                if(st.size()==0)
+                    {
+                        return false;
+                    }
+                if(pref(s[i]) == pref(st.top()))
+                {
+                    
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+       }
+        
+
+        if(st.size()==0)
+        {
+            return true;
+        }
+        return false;
+    }
+};
